27b095db317da2c8b162151aa180d982d5c9de0a
SouDev01 <davilnd8@gmail.com>
2022-11-23 11:23:46 -0300

    incluindo a conexao com o banco de dados

--- src/Connection/DatabaseConnection.php
+++ src/Connection/DatabaseConnection.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace App\Connection;
 
+use PDO;
+
 class DatabaseConnection
 {
-    
+    public static function abriConexao(): PDO
+    {
+        
+        return new PDO("mysql:host=localhost;dbname=" .DB_NAME, DB_USER, DB_PASSWORD);
+    }
 }
