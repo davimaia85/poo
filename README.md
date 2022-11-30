830c0dbc3a5263bb185a5862fa8b4e6be129b37a
SouDev01 <davilnd8@gmail.com>
2022-11-30 09:07:39 -0300

    atualizando a documentação

--- README.md
+++ README.md
@@ -1,26 +1,56 @@
-<h1>Instruções</h1>
-<hr>
-<h3>Descrição</h3>
+#Instruções Crud Escola
 
+###Descrição
+
+Aplicação Web do tipo monolítica criada com:
+- PHP para o backend, ^7.4
+- HTML, CSS, Javascript para o frontend
+- MySQL/ MariaDB para o banco de dados
 Esta aplicação faz a gestão dos elementos que compõe o controle escolar, onde é possível cadastrar, atualizar, excluir e listar os alunos, os professores e os cursos que a escola dispõe. 
 
-<hr>
-<h3>Softwares Necessários</h3>
+###Funcionalidades
 
-PHP
+- CRUD de Alunos
+- CRUD de Professores
+- CRUD de Cursos
+- CRUD de Categorias
+- CRUD de Usuários
 
-MySql
+###Iniciar
 
-Composer
+1- Certifique-se que seu computador tem os softwares:
+    PHP, MySQL ou MariaDB, Editor de texto (ex: VSCode), navegador web (ex: Chrome), Composer
+2- Baixe ou faça o clone do projeto: `git clone ...`;
+3- Entre no diretório: `poo`;
+4- Habilitar as extensões do PHP: 
+        - abra o diretório de instalação do PHP, encontre o arquivo *php.ini-production*, renomei-o
+        para *php.ini* e abra-o com um editor de texto.
+        - encontre as linhas que contém (pdo_mysql, curl, mb-string, openssl) e desconecte-se, removendo ';' que precede a linha
+5- Dentro do diretório da aplicação execute no terminal: `composer install`;
+        - Certifique-se que um diretório **vendor** foi criado 
+6- O banco de dados é do tipo relacional e contém as tabelas com até 2 níveis de normatização;
+7- Gerar o banco de dados: entre no cliente do banco de dados e execute
+    ```sql
+        CREATE DATABASE db_escola
+    ```
+8- Dentro do cliente do banco de dados, copie e cole o conteúdo dop arquivo **db.sql** e execute;
+    - certifique-se que as tabelas forma criadas executando o comando:
+    ```sql
+        SHOW TABLES
+    ```
+    - exibe a lista de tabelas
+9-Cofigurar as credenciais de acesso: 
+    - no arquivo **/config/database.php** e edite as credenciais do seu usuário do banco de dados
 
-<hr>
-<h3>Iniciar</h3>
+###Criar o primeiro usuário de acesso
+1- Dentro do diretório da aplicação, execute o comando `php config/create-admin.php`, para gerar o usuário com as credeniciais:
+| NOME | EMAIL | SENHA |
+| - | - | - |
+| Administrador | admin@admin.com | 123456 |
 
-1- Gerar o banco de dados conforme o arquivo 'bd.sql', utilize MySQL workbench ou MySQL Shell;
-<br>
-2- Ao acessar a pasta principal do projeto, inicie o terminal e execute os comandos 'composer install' e 'php -S localhost:(número da porta) -t public', para inicializar a aplicação.
-<br>
-<b>OBSERVAÇÃO:</b>  na ocorrência do erro 'Class MySqli not found', deve-se fazer uma alteração no arquivo 'php.ini-development', na pasta php instalada, onde faz-se a alteração do nome deste arquivo para 'php.ini' e, no próximo passo deve-se descomentar as linhas 928 e 934 contidadas no conteúdo do referido arquivo, para tal procedimento abra o arquivo usando o VSCode. Após, reinicie a aplicação.
+###Executar a aplicação
+1- Para executar e testar a aplicação, execute no terminal: `php -S localhost:8000 -t public`;
+2- Acesse o endereço http://localhost:8000 no navegador web;
 
 
 
--- db.sql
+++ db.sql
@@ -13,16 +13,6 @@ CREATE TABLE tb_alunos (
     cpf CHAR(11) UNIQUE NOT NULL
 );
 
-INSERT INTO tb_alunos
-(nome, matricula, email, status, genero, dataNascimento, cpf)
-VALUES
-('maria', '1223456', 'maria@email.com', true, 'feminino', '2001-09-04', '12345678900'),
-('ze', '1223457', 'ze@email.com', true, 'feminino', '2001-08-24', '12345678901'),
-('antoin', '1223458', 'antoin@email.com', true, 'feminino', '2001-05-15', '12345678902')
-;
-
-SELECT * FROM tb_alunos;
-
 CREATE TABLE tb_professores (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
@@ -31,16 +21,6 @@ CREATE TABLE tb_professores (
     formacao VARCHAR(100) UNIQUE NOT NULL,
     status TINYINT NOT NULL
 );
-
-INSERT INTO tb_professores
-(nome, cpf, endereco, formacao, status)
-VALUES
-('lorao', '12345678900', 'ruas dos loiros', 'enrolador de codigo', true),
-('allan cahorro e chuchu', '12345678901', 'rua vira latas', 'criados de cachorros e chuchus', false),
-('adirano', '12345678902', 'rua crossfit', 'fazedor de rapadura', true)
-;
-
-SELECT * FROM tb_professores;
 
 CREATE TABLE tb_user (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -51,23 +31,10 @@ CREATE TABLE tb_user (
   
 );
 
-INSERT INTO tb_user
-(name, email, password, profile)
-VALUES
-('loraoDC', 'lorao@email.com', '12345', 'enrolador de codigo'),
-('allan345', 'allan@email.com', '12346', 'criados de cachorros e chuchus'),
-('adrianoDaRapasiada', 'adriano@email.com', '12347', 'fazedor de rapadura')
-;
-
-INSERT INTO tb_user
-(name, email, password, profile)
-VALUES
-('maria', 'maria@email.com','12348', 'legal'),
-('ze', 'ze@email.com',  '12349', 'gente fina'),
-('antoin', 'antoin@email.com', '123410', 'invocado')
-;
-
-SELECT * FROM tb_user;
+CREATE TABLE tb_categorias (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL
+);
 
 CREATE TABLE tb_cursos (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -79,20 +46,59 @@ CREATE TABLE tb_cursos (
     FOREIGN KEY (categoria_id) REFERENCES tb_categorias(id)
 );
 
-CREATE TABLE tb_categorias (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL
-);
+-- INSERT INTO tb_alunos
+-- (nome, matricula, email, status, genero, dataNascimento, cpf)
+-- VALUES
+-- ('maria', '1223456', 'maria@email.com', true, 'feminino', '2001-09-04', '12345678900'),
+-- ('ze', '1223457', 'ze@email.com', true, 'feminino', '2001-08-24', '12345678901'),
+-- ('antoin', '1223458', 'antoin@email.com', true, 'feminino', '2001-05-15', '12345678902')
+-- ;
 
-INSERT INTO tb_categorias (nome) VALUES ('Profissionalizante'), ('Tecnico'), ('Graduação');
+-- SELECT * FROM tb_alunos;
 
-INSERT INTO tb_cursos
-(nome, cargaHoraria, descricao, status, categoria_id)
-VALUES
-('Domador de elefante PHP','200','Desenrolar os bem bolados',1,1),
-('Javasprict','160','Copiar e colar coisas',1,2),
-('HTML e CSS','100','Construir páginas como um pedreiro da web',1,3)
-;
 
-SELECT * FROM tb_cursos INNER JOIN tb_categorias ON tb_cursos.categoria_id = tb_categorias.id;
+
+-- INSERT INTO tb_professores
+-- (nome, cpf, endereco, formacao, status)
+-- VALUES
+-- ('lorao', '12345678900', 'ruas dos loiros', 'enrolador de codigo', true),
+-- ('allan cahorro e chuchu', '12345678901', 'rua vira latas', 'criados de cachorros e chuchus', false),
+-- ('adirano', '12345678902', 'rua crossfit', 'fazedor de rapadura', true)
+-- ;
+
+-- SELECT * FROM tb_professores;
+
+-- INSERT INTO tb_user
+-- (name, email, password, profile)
+-- VALUES
+-- ('loraoDC', 'lorao@email.com', '12345', 'enrolador de codigo'),
+-- ('allan345', 'allan@email.com', '12346', 'criados de cachorros e chuchus'),
+-- ('adrianoDaRapasiada', 'adriano@email.com', '12347', 'fazedor de rapadura')
+-- ;
+
+-- INSERT INTO tb_user
+-- (name, email, password, profile)
+-- VALUES
+-- ('maria', 'maria@email.com','12348', 'legal'),
+-- ('ze', 'ze@email.com',  '12349', 'gente fina'),
+-- ('antoin', 'antoin@email.com', '123410', 'invocado')
+-- ;
+
+-- SELECT * FROM tb_user;
+
+
+
+
+
+-- INSERT INTO tb_categorias (nome) VALUES ('Profissionalizante'), ('Tecnico'), ('Graduação');
+
+-- INSERT INTO tb_cursos
+-- (nome, cargaHoraria, descricao, status, categoria_id)
+-- VALUES
+-- ('Domador de elefante PHP','200','Desenrolar os bem bolados',1,1),
+-- ('Javasprict','160','Copiar e colar coisas',1,2),
+-- ('HTML e CSS','100','Construir páginas como um pedreiro da web',1,3)
+-- ;
+
+-- SELECT * FROM tb_cursos INNER JOIN tb_categorias ON tb_cursos.categoria_id = tb_categorias.id;
 
